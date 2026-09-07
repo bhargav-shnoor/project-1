255f50f686498d678046b0a971bc51f8b8e3bcf5
Arun Siva Bhargav <arunsiva@shnoor.com>
2026-09-07 17:35:08 +0530

    MongoDB to server connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
+
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
