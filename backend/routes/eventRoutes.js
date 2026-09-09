945f2342767824516c124dbc812301cad8f97c13
Arun Siva Bhargav <arunsiva@shnoor.com>
2026-09-09 15:11:45 +0530

    Middleware to protect event routes

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/eventController');
+const protect = require('../middleware/authMiddleware');
+
 router.get('/', getEvents);
-router.post('/', createEvent);
-router.put('/:id', updateEvent);
-router.delete('/:id', deleteEvent);
+
+router.post('/', protect, createEvent);
+router.put('/:id', protect, updateEvent);
+router.delete('/:id', protect, deleteEvent);
+
 module.exports = router;
